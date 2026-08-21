80690e1d777c7e0b857c2dbe7cead54dc2bbed2b
yutaka <yutaka@MuchyutakaHP.lan>
2026-08-21 17:52:08 +0700

    Add OF_DEFAULT_KEYMASTER_VERSION required by TW_FORCE_KEYMASTER_VER

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,3 +154,4 @@ TW_USB_STORAGE := true
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone29/temp"
 TW_CUSTOM_BATTERY_PATH := "/sys/devices/platform/soc/10026000.pwrap/10026000.pwrap:mt6366/mt6358-gauge/power_supply/battery/capacity"
+OF_DEFAULT_KEYMASTER_VERSION := 4
